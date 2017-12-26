e0627b5112f72c079afef0cc4f3e2e6da4c0fc83
takatoh <takatoh.m@gmail.com>
2017-12-26 13:09:58 +0900

    main.go: Calculate and output response spectrum.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/takatoh/respspec/wave"
+	"github.com/takatoh/respspec/response"
 )
 
 func main() {
@@ -12,7 +13,40 @@ func main() {
 
 	wave := wave.LoadCSV(csvfile)
 
+	var freq []float64 = []float64{
+		0.0,
+		0.1,
+		0.2,
+		0.3,
+		0.4,
+		0.5,
+		0.6,
+		0.7,
+		0.8,
+		0.9,
+		1.0,
+		1.1,
+		1.2,
+		1.3,
+		1.4,
+		1.5,
+		1.6,
+		1.7,
+		1.8,
+		1.9,
+		2.0,
+		2.5,
+		3.0,
+		4.0,
+		5.0,
+	}
+	var h float64 = 0.05
+
+	responses := response.Resp(wave, freq, h)
+
 	fmt.Println(wave.Name)
-	fmt.Println(wave.Dt)
-	fmt.Println(len(wave.Data))
+	fmt.Println("Freq,Sa,Sv,Sd")
+	for res := range responses {
+		fmt.Printf("%f,%f,%f,%f\n", res.Freq, res.Sa, res.Sv, res.Sd)
+	}
 }
