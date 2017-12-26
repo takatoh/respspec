f2f925ff1be05755aadc6381d15954735858b1be
takatoh <takatoh.m@gmail.com>
2017-12-26 13:00:26 +0900

    response/response.go: Implement Resp() function.

--- response/response.go
+++ response/response.go
@@ -2,6 +2,8 @@ package response
 
 import (
 	"math"
+
+	"github.com/takatoh/respspec/wave"
 )
 
 type Response struct {
@@ -11,10 +13,75 @@ type Response struct {
 	Sd   float64
 }
 
-func NewResponse(freq float64) *Response {
+func NewResponse(freq, sa, sv, sd float64) *Response {
 	p := new(Response)
 	p.Freq = freq
+	p.Sa = sa
+	p.Sv = sv
+	p.Sd = sd
 	return p
 }
 
+func Resp(wave *Wave, freq []float64, h float64) []*Response {
+	var theta, tdt, omega float64
+	var ath, acd, abz float64
+	var acc, vel, dis float64
+	var a1, a2, a3 float64
+	var am, f, vm, dm float64
+	var k, c float64
 
+	responses := make([]*Response, 0)
+	theta = 1.4
+	tdt = theta * wave.Dt
+	nfreq := len(freq)
+	z := wave.Data
+	n := len(z)
+	dt := wave.Dt
+
+	for j := 0; j < nfreq; j++ {
+		if math.Abs(freq[j]) < 0.01 {
+			am = 0.0
+			for i := 0; i < n; i++ {
+				if math.Abs(z[i]) > am {
+					am = math.Abs(z[i])
+				}
+			}
+			responses = append(responses, NewResponse(freq[j], am, 0.0, 0.0))
+		} else {
+			omega = 2.0 * math.Pi / freq[j]
+			k = omega * omega
+			c = 2.0 * h * omega
+
+			// Constants for Willson's theta method.
+			a1 = 1.0 + tdt * c / 2.0 + k * tdt * tdt / 6.0
+			a2 = c + k * tdt
+			a3 = tdt * c / 2.0 + k / 3.0 * tdt * tdt
+
+			// Set initial values.
+			acc = 0.0
+			vel = 0.0
+			dis = 0.0
+			am = 0.0
+			vm = 0.0
+			dm = 0.0
+
+			// Willson's theta method.
+			for i := 0; i < n - 1; i++ {
+				f = (theta - 1.0) * z[i] - theta * z[i + 1]
+				ath = (f - k * dis - a2 * vel - a3 * acc) / a1
+				acd = ((theta - 1.0) * acc + ath) / theta
+				dis = dis + dt * vel + acc * dt * dt / 3.0 + acd * dt * dt / 6.0
+				vel = vel + (acc + acd) * dt / 2.0
+				acc = acd
+				abz = math.Abs(acc + z[i + 1])
+				if abz > am { am = abz }
+				if math.Abs(vel) > vm { vm = math.Abs(vel) }
+				if math.Abs(dis) > dm { dm = math.Abs(dis) }
+			}
+
+			responses = append(responses, NewResponse(freq[j], am, vm, dm))
+		}
+	}
+
+	return responses
+}
